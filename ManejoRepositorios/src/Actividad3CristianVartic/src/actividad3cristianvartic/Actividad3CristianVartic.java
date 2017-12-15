a164f5dacfc666b374aefdfe0bc6b581b7b67a00
daw124 <34559382+daw124@users.noreply.github.com>
2017-12-15 09:42:15 +0100

    Add files via upload

---
+++ ManejoRepositorios/src/Actividad3CristianVartic/src/actividad3cristianvartic/Actividad3CristianVartic.java
@@ -0,0 +1,16 @@
+
+package actividad3cristianvartic;
+
+/**
+ *
+ * @author Chris
+ */
+public class Actividad3CristianVartic {
+
+    public static void main(String[] args) {
+       int n = 124;
+        System.out.println("Entorno desarollo " + n );
+      
+    }
+    
+}
